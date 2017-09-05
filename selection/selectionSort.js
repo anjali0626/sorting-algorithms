fda9c4c687e4ca28b86583a9939c55071257e9b0
anjali0626 <a9sharma@scu.edu>
2017-09-05 14:17:53 -0700

    Sort array using Selection Sort

---
+++ selection/selectionSort.js
@@ -0,0 +1,26 @@
+// WAP to sort a given array using Selection Sort
+
+function selectionSort(arr) {
+
+  for (var i = 0; i < arr.length; i += 1) {
+
+    var minIndex = i;
+
+    for (var j = i + 1; j < arr.length; j += 1) {
+
+      if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+      }
+
+    }
+
+    var temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+
+  }
+
+  return arr;
+}
+
+selectionSort([100, 0, 73, 45, -1, -20, -190, 1000]); // [-190, -20, -1, 0, 45, 73, 100, 1000]
