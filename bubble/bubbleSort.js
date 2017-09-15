3272a5a06894c5bd7052440b18d8db76373c3263
anjali0626 <a9sharma@scu.edu>
2017-09-14 18:36:28 -0700

    Optimize Bubble Sort

--- bubble/bubbleSort.js
+++ bubble/bubbleSort.js
@@ -2,8 +2,8 @@
 
 function bubbleSort(arr) {
 
-  for (var i = 0; i < arr.length; i += 1) {
-    for (var j = 0; j < arr.length; j += 1) {
+  for (var i = 0; i < arr.length - 1; i += 1) {
+    for (var j = 0; j < arr.length - 1 - i; j += 1) {
       if (arr[j] > arr[j + 1]) {
         var temp = arr[j];
         arr[j] = arr[j + 1];
